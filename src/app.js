13817a43e4ed24a19c75879214049d45e058b16e
petr.m <petr.dev@gmail.com>
2018-11-05 13:40:09 +0300

    add modules to ignore

---
+++ src/app.js
@@ -0,0 +1,7 @@
+/*const appFragment  = document.createDocumentFragment();
+const appContiner = document.createElement('div');
+appContiner.id = 'menu_app';
+appContiner.className = 'menu-app'
+appFragment.appendChild(appContiner)
+document.body.appendChild(appFragment);*/
+console.log(`I'm a silly entry point`);
---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+console.log(HtmlWebpackPlugin);
+
+console.log('WEBPACK START')
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+
+				use: [
+					{
+						loader: 'style-loader',
+
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'css-loader'
+					}
+				]
+			},
+			// {
+			// 	test: /\.html$/,
+			// 	use: [
+			// 	  {
+			// 		loader: "html-loader",
+			// 		options: { minimize: true }
+			// 	  }
+			// 	]
+			//   }
+		]
+	},
+
+	entry: {
+		app: './src/app.js',
+		vendor: './src/vendor.js'
+	},
+
+	output: {
+		filename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	mode: 'development',
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			hash: true,
+			template: './src/index.html',
+			filename: 'index.html'
+		}
+		),
+		new UglifyJSPlugin(),
+	],
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					priority: -10,
+					test: /[\\/]node_modules[\\/]/
+				}
+			},
+
+			chunks: 'async',
+			minChunks: 1,
+			minSize: 30000,
+			name: true
+		}
+	}
+};
